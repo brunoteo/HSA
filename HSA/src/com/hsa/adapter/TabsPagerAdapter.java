683dfd017c66069e400a5465d644681650c62d45
brunetto matteo <m.brunetto3@campus.unimib.it>
2014-05-05 14:46:47 +0200

    Prima schermata!

---
+++ HSA/src/com/hsa/adapter/TabsPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.hsa.adapter;
+
+import com.hsa.fragment.DecksFragment;
+import com.hsa.fragment.NewsFragment;
+import com.hsa.fragment.SearchFragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter{
+
+	public TabsPagerAdapter(FragmentManager fm) {
+		super(fm);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Fragment getItem(int index) {
+		
+		switch (index) {
+        case 0:
+            // Top Rated fragment activity
+            return new NewsFragment();
+        case 1:
+            // Games fragment activity
+            return new SearchFragment();
+        case 2:
+            // Movies fragment activity
+            return new DecksFragment();
+        }
+ 
+        return null;
+	}
+
+	@Override
+	public int getCount() {
+
+		return 3;
+	}
+
+}
---
+++ HSA/src/com/hsa/fragment/DecksFragment.java
@@ -0,0 +1,20 @@
+package com.hsa.fragment;
+
+import com.hsa.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DecksFragment extends Fragment{
+
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_decks, container, false);
+         
+        return rootView;
+    }
+}
