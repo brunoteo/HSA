d8ece1d0cb226c94ece91eecd8fc710f0fb5cd6e
brunetto matteo <m.brunetto3@campus.unimib.it>
2014-05-14 13:30:29 +0200

    AggregazioneGrafica V2

---
+++ HSA/src/com/hsa/aggregation/GraphicalAggregation.java
@@ -0,0 +1,31 @@
+package com.hsa.aggregation;
+
+import android.graphics.drawable.Drawable;
+
+public class GraphicalAggregation {
+	
+	private String name;
+	private String occurence;
+	private Drawable image;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getOccurence() {
+		return occurence;
+	}
+	public void setOccurence(String occurence) {
+		this.occurence = occurence;
+	}
+	public Drawable getImage() {
+		return image;
+	}
+	public void setImage(Drawable image) {
+		this.image = image;
+	}
+	
+
+}
