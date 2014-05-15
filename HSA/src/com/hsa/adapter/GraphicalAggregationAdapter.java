4f624dde2039f4a954df8072e20413e1d49fc4dc
brunetto matteo <m.brunetto3@campus.unimib.it>
2014-05-15 14:10:53 +0200

    AggregazioneGrafica V3

--- HSA/src/com/hsa/adapter/GraphicalAggregationAdapter.java
+++ HSA/src/com/hsa/adapter/GraphicalAggregationAdapter.java
@@ -6,10 +6,12 @@ import com.hsa.R;
 import com.hsa.aggregation.GraphicalAggregation;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,10 +19,12 @@ public class GraphicalAggregationAdapter extends BaseAdapter{
 
 	private final List<GraphicalAggregation> grapichalsAggregations;
 	private LayoutInflater inflater;
+	private Context context;
  
 	public GraphicalAggregationAdapter(Context context, List<GraphicalAggregation> ga) {
 		this.inflater = LayoutInflater.from(context);
 		this.grapichalsAggregations = ga;
+		this.context = context;
 	}
 	
 	@Override
@@ -64,29 +68,42 @@ public class GraphicalAggregationAdapter extends BaseAdapter{
 //		return gridView;
 //	}
 	
+//	@Override
+//	public View getView(int i, View view, ViewGroup viewGroup) {
+//		
+//		 View v = view;
+//         ImageView picture;
+//         TextView name;
+//
+//         if(v == null)
+//         {
+//            v = inflater.inflate(R.layout.gridview_item, viewGroup, false);
+//            v.setTag(R.id.picture, v.findViewById(R.id.picture));
+//            v.setTag(R.id.text, v.findViewById(R.id.text));
+//         }
+//
+//         picture = (ImageView)v.getTag(R.id.picture);
+//         name = (TextView)v.getTag(R.id.text);
+//
+//         GraphicalAggregation graphicalAggregation = grapichalsAggregations.get(i);
+//
+//         picture.setImageResource(graphicalAggregation.getImage());
+//         if(graphicalAggregation.getOccurence()==0) {
+//        	 name.setBackgroundColor(Color.parseColor("#07000000"));
+//         } else {
+//        	 name.setText(Integer.toString(graphicalAggregation.getOccurence()));
+//         }
+//         
+//
+//         return v;
+//	}
+
 	@Override
-	public View getView(int i, View view, ViewGroup viewGroup) {
-		
-		 View v = view;
-         ImageView picture;
-         TextView name;
-
-         if(v == null)
-         {
-            v = inflater.inflate(R.layout.gridview_item, viewGroup, false);
-            v.setTag(R.id.picture, v.findViewById(R.id.picture));
-            v.setTag(R.id.text, v.findViewById(R.id.text));
-         }
-
-         picture = (ImageView)v.getTag(R.id.picture);
-         name = (TextView)v.getTag(R.id.text);
-
-         GraphicalAggregation graphicalAggregation = grapichalsAggregations.get(i);
-
-         picture.setImageResource(graphicalAggregation.getImage());
-         name.setText("3");
-
-         return v;
-	}
-
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(context);
+        imageView.setImageResource(grapichalsAggregations.get(position).getImage());
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new GridView.LayoutParams(70, 100));
+        return imageView;
+    }
 }
