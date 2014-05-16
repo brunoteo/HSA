1c3171002ba08b0377c01f008a54ed437e914678
Dafian <Dafian@149.132.188.240>
2014-05-16 14:21:41 +0200

    deck fragments little things

---
+++ HSA/src/com/hsa/aggregation/DeckDataAggregation.java
@@ -0,0 +1,35 @@
+package com.hsa.aggregation;
+
+public class DeckDataAggregation {
+
+	private String nome;
+	private String className;
+	private int cardNumber;
+	private String date;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public int getCardNumber() {
+		return cardNumber;
+	}
+	public void setCardNumber(int cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+}
--- HSA/src/com/hsa/fragment/DecksFragment.java
+++ HSA/src/com/hsa/fragment/DecksFragment.java
@@ -1,6 +1,12 @@
 package com.hsa.fragment;
 
+import java.util.List;
+
+import com.hsa.MainActivity;
 import com.hsa.R;
+import com.hsa.aggregation.GraphicalAggregation;
+import com.hsa.manager.ViewManager;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -17,4 +23,5 @@ public class DecksFragment extends Fragment{
          
         return rootView;
     }
+	
 }
