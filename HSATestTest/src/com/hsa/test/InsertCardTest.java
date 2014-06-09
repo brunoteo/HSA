2f99c183460a8eda03a27e50914e184adddf4d39
brunetto matteo <m.brunetto3@campus.unimib.it>
2014-06-09 12:46:09 +0200

    Prova test

--- HSATestTest/src/com/hsa/test/InsertCardTest.java
+++ HSATestTest/src/com/hsa/test/InsertCardTest.java
@@ -1,19 +1,17 @@
 package com.hsa.test;
 
-import com.hsa.activity.DeckActivity;
+import com.hsa.MainActivity;
 import com.hsa.database.HSADatabaseHelper;
 import com.hsa.handler.DeckHandler;
 
 import android.test.ActivityInstrumentationTestCase2;
 
-public class InsertCardTest extends
-		ActivityInstrumentationTestCase2<DeckActivity> {
-	
-	private DeckHandler deckHandler;
+public class InsertCardTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-	public InsertCardTest(Class<DeckActivity> activityClass) {
-		super(activityClass);
-		// TODO Auto-generated constructor stub
+	private DeckHandler deckHandler;
+	
+	public InsertCardTest() {
+		super(MainActivity.class);
 	}
 
 	protected void setUp() throws Exception {
@@ -21,11 +19,12 @@ public class InsertCardTest extends
 		deckHandler = DeckHandler.getInstance(HSADatabaseHelper.getInstance(getActivity()));
 	}
 	
-	public void testDio() {
-		if(deckHandler.getCopyFormations().size() == 1) {
-			assertTrue(true);
-			
-		}
+	public void test1() {
+		assertTrue(deckHandler!=null);
+	}
+	
+	public void test2() {
+		assertTrue(deckHandler.getCopyFormations()!=null);
 	}
 
 }
